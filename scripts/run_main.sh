c93e9b1b8745d68932880a4629e839a414a759cf
Niksa Praljak <niksapraljak1@midway3-login4.rcc.local>
2023-12-10 11:55:02 -0600

    Upload test data inputs and outputs, cleanup shell scripts, and update gitignore

--- scripts/run_main.sh
+++ scripts/run_main.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
-export DIR="$(dirname "$(pwd)")"
+export DIR="$(dirname "$(pwd)")" # Parent directory of the current working directory
+export input_fasta_path='/project/andrewferguson/niksapraljak/Minimalist_Codon_Translation/outputs/output_SH3_single_DNA_test.fasta' # Path to input FASTA file
+export output_fasta_path='/project/andrewferguson/niksapraljak/Minimalist_Codon_Translation/outputs/output_SH3_single_optimized_DNA_test.fasta' # Path to output FASTA file
+export lim_GC_high=0.6 # Upper limit for GC content
+export lim_GC_low=0.45 # Lower limit for GC content
+export codon_bias_path='/project/andrewferguson/niksapraljak/Minimalist_Codon_Translation/Codon_bias/YeastProteomeContent.tsv' # Path to codon bias file
+export seed=42 # Seed value for reproducibility in random processes
 
-export input_fasta_path='/project/andrewferguson/niksapraljak/Minimalist_Codon_Translation/outputs/output_SH3_single_DNA_test.fasta'
-export output_fasta_path='/project/andrewferguson/niksapraljak/Minimalist_Codon_Translation/outputs/output_SH3_single_optimized_DNA_test.fasta'
-export lim_GC_high=0.6 # default: 0.60
-export lim_GC_low=0.45 # default: 0.45
-export codon_bias_path='/project/andrewferguson/niksapraljak/Minimalist_Codon_Translation/Codon_bias/YeastProteomeContent.tsv' # codon frequency file (depends on specie)
-export seed=42
 
 python ../main.py \
 	--input ${input_fasta_path} \
--- scripts/run_reverse_aa_translation.sh
+++ scripts/run_reverse_aa_translation.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env sh
 
-export DIR="$(dirname "$(pwd)")"
-
-export input_fasta_path='/project/andrewferguson/niksapraljak/Minimalist_Codon_Translation/inputs/SH3_test.fasta'
-export output_fasta_path='/project/andrewferguson/niksapraljak/Minimalist_Codon_Translation/outputs/output_SH3_DNA_test.fasta' # take 'fasta', 'csv', or 'tsv
-
-export codon_bias_path='/project/andrewferguson/niksapraljak/Minimalist_Codon_Translation/Codon_bias/YeastProteomeContent.tsv' # codon frequency file (depends on specie)
-export seed=42
-export aa_header='amino_acid'
-export codon_header='codon'
-export frequency_header='norm_freq'
+export DIR="$(dirname "$(pwd)")" # Parent directory of the current working directory
+export input_fasta_path='/project/andrewferguson/niksapraljak/Minimalist_Codon_Translation/inputs/SH3_test.fasta' # Path to input FASTA file
+export output_fasta_path='/project/andrewferguson/niksapraljak/Minimalist_Codon_Translation/outputs/output_SH3_DNA_test.fasta' # Path to output file (fasta, csv, tsv)
+export codon_bias_path='/project/andrewferguson/niksapraljak/Minimalist_Codon_Translation/Codon_bias/YeastProteomeContent.tsv' # Path to codon bias file
+export seed=42 # Seed value for reproducibility in random processes
+export aa_header='amino_acid' # Header for amino acid in output
+export codon_header='codon' # Header for codon in output
+export frequency_header='norm_freq' # Header for frequency in output
 
 
 python ../reverse_translation_main.py \
